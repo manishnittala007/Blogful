19981cd9b9b30d45c49c094c1897d7ce51d6fdf2
maximus <88191499+manishnittala007@users.noreply.github.com>
2024-03-05 17:30:21 +0530

    Update individual.js

--- individual.js
+++ individual.js
@@ -80,7 +80,7 @@ function createBackArrow() {
     backArrow.style.cursor = 'pointer';
 
     var backImg = document.createElement('img');
-    backImg.src = '/images/backarrow.png';
+    backImg.src = 'images/backarrow.png';
     backImg.style.height = "25px";
     backArrow.appendChild(backImg);
 
@@ -158,7 +158,7 @@ function createImgDiv() {
     imgDiv.style.position = 'relative';
     imgDiv.style.display = 'inline-block';
     const shareImage = document.createElement('img');
-    shareImage.src = '/images/share.png';
+    shareImage.src = 'images/share.png';
     shareImage.classList.add('mx-3', 'my-3');
     shareImage.style.opacity = '0.6';
     shareImage.style.cursor = 'pointer';
